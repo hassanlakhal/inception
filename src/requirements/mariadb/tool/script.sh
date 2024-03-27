264958864b933f6e2b77e00d975e623ec81b8a3e
Hassan Lakhal <hlakhal-@e2r2p10.1337.ma>
2024-03-27 01:41:45 +0000

    add scrpit of data bases

--- src/requirements/mariadb/tool/script.sh
+++ src/requirements/mariadb/tool/script.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 service mariadb start
 
+sleep 2
+
+service mariadb stop
+
 mariadb << "EOF
+    create database $DB_NAME;
+    cretae user '$MYSQL_USER'@'localhost' identified by '$MYSQL_PASSWORD';
+# EOF"
+
+exec mysqld_safe
