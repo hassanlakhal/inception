ee0e8fd3c6b566d9e06fae1835fd551cdd5e6364
lakhlhassan <lakhelhassan2120@gmail.com>
2024-04-01 03:39:50 +0000

    add wordpress

---
+++ src/requirements/wordpress/tool/script.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+mkdir /var/www/html/wordpress
+
+workdir /var/www/html/wordpress
+
+wget https://wordpress.org/latest.tar.gz
+
+tar -xvf latest.tar.gz
+
+# mv wordpress/* /var/www/html/wordpress/
+
+rm -rf latest.tar.gz
+
+# chown -R www-data:www-data /var/www/html/wordpress/*
+
+# chmod -R 755 /var/www/html/wordpress/*
+
+# tail -f
+exec php-fpm7.4  -F
+# php-fpm -RF
+
+
+
