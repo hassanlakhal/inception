35209d5aa0d44da5d4e337513b31475d9e61a9f4
lakhlhassan <lakhelhassan2120@gmail.com>
2024-04-03 22:35:43 +0000

    link mariadb with wordpress is done

--- src/requirements/wordpress/tool/script.sh
+++ src/requirements/wordpress/tool/script.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 mkdir /var/www/html/wordpress
-
 wget https://wordpress.org/latest.tar.gz
 
 tar -xvf latest.tar.gz
@@ -20,10 +19,25 @@ chmod +x wp-cli.phar
 
 mv wp-cli.phar /usr/local/bin/wp
 
-sleep 5
+cd  /var/www/html/wordpress
 
-wp config create --allow-root --dbname=$DB_NAME --dbuser=$DB_USER \
-    --dbpass=$DB_PASS --dbhost=mariadb:3306 --path='/var/www/html/wordpress'
+sleep 10
+
+sed -i "s/database_name_here/$DB_NAME/g"  wp-config-sample.php
+
+sed -i "s/username_here/$DB_USER/g"  wp-config-sample.php
+
+sed -i "s/password_here/$DB_PASS/g"  wp-config-sample.php
+
+sed -i "s/localhost/mariadb:3306/g"  wp-config-sample.php
+
+cp wp-config-sample.php wp-config.php
+# printf "define('DB_NAME', 'wordpress_db');
+#  define('DB_USER', 'wpuser');
+#  define('DB_PASSWORD', 'Passw0rd!');" > wp-config.php
+
+# wp config create --allow-root --dbname=$DB_NAME --dbuser=$DB_USER \
+#     --dbpass=$DB_PASS --dbhost=mariadb:3306 --path='/var/www/html/wordpress'
 
 exec php-fpm7.4  -F
 
