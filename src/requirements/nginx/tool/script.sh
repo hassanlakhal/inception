254c9f8ba95a08a063e952b39a93ceb2f3ac7688
lakhalhassan <lakehalhassan2120@gmail.com>
2024-04-26 16:47:56 +0100

    last mod

--- src/requirements/nginx/tool/script.sh
+++ src/requirements/nginx/tool/script.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-openssl req -x509 -nodes -keyout /etc/nginx/ssl/inception.key -out /etc/nginx/ssl/inception.crt \
+openssl req -x509 -nodes -keyout $KEY_S -out $CRT_S \
     -subj "/C=MA/ST=bengrir/L=bengrir/O=1337/OU=1337 Unit/CN=hlakhal-"
 
-sleep 10 
+cd /etc/nginx/sites-enabled/
+sed -i "s#crtPath#$CRT_S#g" default
+sed -i "s#keyPath#$KEY_S#g" default
+sleep 10
 exec nginx -g "daemon off;"
