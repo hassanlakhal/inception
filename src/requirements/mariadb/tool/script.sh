6b463969511bf00b356ec68bfc1451bf75b11908
lakhalhassan <lakehalhassan2120@gmail.com>
2024-10-17 23:02:54 +0100

    change host of connecte of data bases

--- src/requirements/mariadb/tool/script.sh
+++ src/requirements/mariadb/tool/script.sh
@@ -7,8 +7,8 @@ if ! mariadb -u"$DB_USER" -p"$DB_PASS" -e \
 "SELECT 1 FROM mysql.user WHERE user='$DB_USER'" &> /dev/null; then
     mariadb -u"$DB_USER" -p"$DB_PASS" <<EOF
         CREATE DATABASE $DB_NAME;
-        CREATE USER '$DB_USER'@'%' IDENTIFIED BY '$DB_PASS';
-        GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%';
+        CREATE USER '$DB_USER'@'wordpress' IDENTIFIED BY '$DB_PASS';
+        GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'wordpress';
         FLUSH PRIVILEGES;
 EOF
 else
